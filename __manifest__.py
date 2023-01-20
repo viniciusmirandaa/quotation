cb763aeacd10760d9f61f9497c334ce911a55553
vinicius <vini.s.miranda@hotmail.com>
2023-01-20 10:30:31 -0300

    [ADD] adding last changes

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': "Quotation",
+    'summary': "Quotation Creation",
+    'author': "Vinicius Miranda",
+    "contributors": [
+    ],
+    'website': "https://vidroshima.com.br/",
+    'category': 'Uncategorized',
+    'version': '14.0.1.0.0',
+    'depends': ['base'],
+    'data': [
+        "security/groups.xml",
+        "security/ir.model.access.csv",
+        "wizards/quotation_wizard_view.xml",
+    ],
+    'installable': True,
+    'application': True,
+}
--- wizards/quotation_wizard.py
+++ wizards/quotation_wizard.py
@@ -9,7 +9,8 @@ class QuotationWizard(models.TransientModel):
         selection=[
             ('customer', 'Customer'),
             ('route', 'Route')
-        ]
+        ],
+        default=False
     )
 
     partner_id = fields.Many2one(
@@ -18,14 +19,27 @@ class QuotationWizard(models.TransientModel):
         required=True
     )
 
-    customer_route = fields.Many2one(
+    route_id = fields.Many2one(
         string="Route",
         comodel_name="routes",
-        default=lambda lm: lm._default_customer_route()
     )
 
-    def _default_customer_route(self):
-        if self.partner_id:
-            return self.partner_id.route_id.id
+    @api.onchange('route_id')
+    def _onchange_customer_selection(self):
+        if self.customer_selection == 'route':
+            customer_ids = self.env['res.partner'].search([('route_id', '=', self.route_id.id)])
 
-    
+            return {'domain': {
+                'partner_id': [('id', 'in', customer_ids.ids)]
+            }}
+
+    @api.onchange('partner_id')
+    def _onchange_partner_id(self):
+        if self.partner_id:
+            self.route_id = self.partner_id.route_id.id
+
+    @api.onchange('customer_selection')
+    def _onchange_customer_selection(self):
+        if self.customer_selection:
+            self.route_id, self.partner_id = False
+
